ed9dfa62b9f6109e4fe079cac405e55d6a01ff9c
Hershey Marbeda <marbedahershey@gmail.com>
2025-09-05 13:49:24 -0400

    Final Changes Made

---
+++ README.md
@@ -0,0 +1,10 @@
+# casaiya_final_showcase
+Final Showcase Website for Casaiya without backend. Simply Straight Forward html/css/jscripts. 
+
+## Resources
+- Fonts from Google Fonts
+- HTML/CSS styling inspirations from Bootstrap
+- Icons from Bootstrap
+
+## Issues (as of 9/5/2025)
+- Not fully done
---
+++ script.js
@@ -0,0 +1,51 @@
+// Navigation menu toggle for hamburger
+function toggleMenu() {
+  const menuLinks = document.querySelector('.menu-links');
+  const hamburgerIcon = document.querySelector('.hamburger-icon');
+
+  menuLinks.classList.toggle('open');
+  hamburgerIcon.classList.toggle('open'); 
+}
+
+// Disable right-click context menu on the video
+const video = document.getElementById('casaiya');
+video.addEventListener('contextmenu', (e) => e.preventDefault());
+
+// Disable Picture-in-Picture (PiP)
+video.disablePictureInPicture = true;
+
+// Disable keyboard shortcuts for playback speed
+video.addEventListener('ratechange', () => {
+  video.playbackRate = 1; // Force playback speed to normal
+});
+
+// Navigation hide/show on scroll
+let lastScrollTop = 0;
+const desktopNav = document.getElementById('desktop-nav');
+const hamburgerNav = document.getElementById('hamburger-nav');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        if (desktopNav) {
+            desktopNav.classList.add('hide-nav');
+            desktopNav.classList.remove('show-nav');
+        }
+        if (hamburgerNav) {
+            hamburgerNav.classList.add('hide-nav');
+            hamburgerNav.classList.remove('show-nav');
+        }
+    } else {
+        // Scrolling up
+        if (desktopNav) {
+            desktopNav.classList.remove('hide-nav');
+            desktopNav.classList.add('show-nav');
+        }
+        if (hamburgerNav) {
+            hamburgerNav.classList.remove('hide-nav');
+            hamburgerNav.classList.add('show-nav');
+        }
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+}, false);
